55387ad72cef7aa772c6b5a7a79cacd3ccdebd6c
ZeroBug <404.zerobug@gmail.com>
2022-02-17 01:20:58 +0600

    without save section done

--- app.js
+++ app.js
@@ -1,20 +1,39 @@
-document.getElementById('calc-btn').addEventListener('click', function () {
-    const foodInput = document.getElementById('food-input').value;
-    const rentInput = document.getElementById('rent-input').value;
-    const clothInput = document.getElementById('cloth-input').value;
-    const foodCost = parseInt(foodInput);
-    const rentCost = parseInt(rentInput);
-    const clothCost = parseInt(clothInput);
+document.getElementById("calc-btn").addEventListener("click", function () {
+  // get expences input values
+  const foodInput = document.getElementById("food-input").value;
+  const rentInput = document.getElementById("rent-input").value;
+  const clothInput = document.getElementById("cloth-input").value;
+  const foodCost = parseInt(foodInput);
+  const rentCost = parseInt(rentInput);
+  const clothCost = parseInt(clothInput);
 
-    const totalCost = foodCost + rentCost + clothCost;
+  const totalCost = foodCost + rentCost + clothCost;
 
-    const totalExpence = document.getElementById('total-expence');
+  //   get incomes input value
+  const incomeInput = document.getElementById("income-input").value;
+  const incomeAmount = parseInt(incomeInput);
+  const balance = incomeAmount - totalCost;
+  
+    // get wrong typing error
+  const typedString = document.getElementById("typed-string");
+  const typedNegative = document.getElementById("typed-negetive");
+  if (isNaN(totalCost) || isNaN(balance)) {
+    
+    typedString.style.display = "block";
+    typedNegative.style.display = "none";
+  } 
+  else if(foodCost < 0 || rentCost < 0 || clothCost < 0 || incomeAmount < 0) {
+    typedNegative.style.display = "block";
+    typedString.style.display = "none";
+  }
+  else {
+      //   update total expences
+    const totalExpence = document.getElementById("total-expence");
     totalExpence.innerText = totalCost;
 
-    const incomeInput = document.getElementById('income-input').value;
-    const incomeAmount = parseInt(incomeInput)
-    const balance = incomeAmount - totalCost;
-
-    const totalBalance = document.getElementById('total-balance');
+    // update balance
+    const totalBalance = document.getElementById("total-balance");
     totalBalance.innerText = balance;
-})
+  }
+  
+});
