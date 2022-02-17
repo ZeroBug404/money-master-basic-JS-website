ec6971c5f28c3180811a8677c9e558cc17b0db29
ZeroBug <404.zerobug@gmail.com>
2022-02-17 16:53:55 +0600

    Did some fixes only the last error message left

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-function getIncomeInput() {
+// get the incone input
+function getIncomeAmount() {
   const incomeInput = document.getElementById("income-input").value;
   const incomeAmount = parseInt(incomeInput);
 
@@ -15,8 +16,14 @@ document.getElementById("calc-btn").addEventListener("click", function () {
   const clothCost = parseInt(clothInput);
 
   const totalCost = foodCost + rentCost + clothCost;
+
+  const balance = getIncomeAmount() - totalCost;
+
+  // total expence & balance 
+  const totalExpence = document.getElementById("total-expence");
+  const totalBalance = document.getElementById("total-balance");
+
   
-  const balance = getIncomeInput() - totalCost;
 
   // get wrong typing error
   const typedString = document.getElementById("typed-string");
@@ -28,32 +35,38 @@ document.getElementById("calc-btn").addEventListener("click", function () {
     foodCost < 0 ||
     rentCost < 0 ||
     clothCost < 0 ||
-    getIncomeInput() < 0
+    getIncomeAmount() < 0
   ) {
     typedNegative.style.display = "block";
     typedString.style.display = "none";
-  } else {
-    //   update total expences
-    const totalExpence = document.getElementById("total-expence");
+  } else {  
+      //   update total expences
     totalExpence.innerText = totalCost;
-
+    
     // update balance
-    const totalBalance = document.getElementById("total-balance");
     totalBalance.innerText = balance;
-  }
+    
+  };
+
+  // another error message 
+  // const exceedIncome = document.getElementById("exceed-income");
+  // if (totalBalance < 0) {
+  //   // console.log(exceedIncome);
+  //   exceedIncome.style.display = "block";
+  // }
+  
 });
 
 document.getElementById("save-btn").addEventListener("click", function () {
-    getIncomeInput();
+  // get saving percenteges inputs and amounts
   const inputPercentege = document.getElementById("input-percentege").value;
-  const savingAmountNumber = document.getElementById('saving-amount');
-  const remainingBalance =document.getElementById('remaining-balance');
+  const savingAmountNumber = document.getElementById("saving-amount");
+  const remainingBalance = document.getElementById("remaining-balance");
   const percentegeNumber = parseInt(inputPercentege);
 
-  const saveAmountTotal = getIncomeInput() / percentegeNumber;
+  const saveAmountTotal = (getIncomeAmount() / 100) * percentegeNumber;
 
-  savingAmountNumber.innerText = saveAmountTotal;
-
+  // get expences input values and balance
   const foodInput = document.getElementById("food-input").value;
   const rentInput = document.getElementById("rent-input").value;
   const clothInput = document.getElementById("cloth-input").value;
@@ -62,10 +75,21 @@ document.getElementById("save-btn").addEventListener("click", function () {
   const clothCost = parseInt(clothInput);
 
   const totalCost = foodCost + rentCost + clothCost;
-  
-  const balance = getIncomeInput() - totalCost;
+
+  const balance = getIncomeAmount() - totalCost;
+
+  // err message
+  const errMessege = document.getElementById("over-balanced");
+  if (saveAmountTotal > balance) {
+    errMessege.style.display = "block";
+  } else {
+    // set saving amont
+    savingAmountNumber.innerText = saveAmountTotal;
+    errMessege.style.display = "none";
+  }
 
   const remainingAmount = balance - saveAmountTotal;
 
+  // set remaining balance
   remainingBalance.innerText = remainingAmount;
 });
